eb88ff090144b468c7ad865f4db4aaff5101fbbe
atharva-gadekar <atharvagadekar@gmail.com>
2022-01-16 15:56:43 +0530

    Task 3 Completed

--- atharva-gadekar.py
+++ atharva-gadekar.py
@@ -1,2 +1,5 @@
 # My favourite show is Attack on Titan
 # It has an amazing plot, which ism difficult to understand easily
+# Sharvari
+# Y
+# Amazing experience! Got to learn a lot about GitHub. Thanks to our seniors for such immense efforts.
